5a79485d3142ca0d492ff0537ac1caa9ac7c4072
Tarlana Harini <157625961+TarlanaHarini@users.noreply.github.com>
2025-05-15 15:42:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import joblib
+import os
 
 app = Flask(__name__)
 model = joblib.load('sentiment_model.pkl')
@@ -15,4 +16,5 @@ def predict():
     return render_template('index.html', input=text, result=prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
